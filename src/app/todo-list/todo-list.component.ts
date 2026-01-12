77d0604e8380136c641d10bbafa224f084aa54ca
MAULIK PATEL <maulikspatel295@gmail.com>
2026-01-12 17:54:40 +0530

    One Way data binding withe interface

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Task } from '../model/task';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-
+  task: Task = { id: 1, title: 'First Task', date: new Date('2026-03-31') };
 }
